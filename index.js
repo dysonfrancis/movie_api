f0cf712d3a00d9d0bb7a5705a7827d422a4c8244
dysonfrancis <78385690+dysonfrancis@users.noreply.github.com>
2021-08-20 16:37:32 -0400

    2.10 Updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Uploaded Aug 20
 
 
-const express = require("express");
+const express = require("express"); 
 
 
 
@@ -35,7 +35,8 @@ require('./passport');
 const { check, validationResult } = require('express-validator');
 
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Static file path
 app.use(express.static('public'));
