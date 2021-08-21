48c36c2bf4fe778ae317b96672fb9bccfe3c3fff
dysonfrancis <78385690+dysonfrancis@users.noreply.github.com>
2021-08-20 23:44:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ app.delete('/users/:Username'  , passport.authenticate('jwt', {session: false}),
     });
 });
 
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
